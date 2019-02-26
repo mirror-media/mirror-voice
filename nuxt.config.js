ebd5c127780a54b11bd1d541522ec9695beee392
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-02-26 19:10:22 +0800

    adjust @nuxt/axios options

--- nuxt.config.js
+++ nuxt.config.js
@@ -77,6 +77,8 @@ module.exports = {
   */
   axios: {
     // See https://github.com/nuxt-community/axios-module#options
+    host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : '127.0.0.1',
+    port: process.env.NODE_ENV === 'production' ? 80 : 8080
   },
 
   /*
