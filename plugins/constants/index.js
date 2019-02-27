b51bfe937be049e1abe9d2e2232eaf97b6d37b6a
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-02-27 11:18:58 +0800

    create MAX_RESULTS as constants

--- plugins/constants/index.js
+++ plugins/constants/index.js
@@ -1,5 +1,11 @@
 import SANITIZE_HTML_DEFAULT_OPTIONS from './sanitizeHtml'
+import * as MAX_RESULTS from './maxResults'
 
 export default (context, inject) => {
   inject('SANITIZE_HTML_DEFAULT_OPTIONS', SANITIZE_HTML_DEFAULT_OPTIONS)
+  Object.entries(MAX_RESULTS).forEach(pair => {
+    const key = pair[0]
+    const value = pair[1]
+    inject(key, value)
+  })
 }
