9d9a4f75365c48d23c7e4a99adb35a4cf10d0261
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-03-20 22:29:09 -0500

    feat: separate read/write from redis, get audio sections from config (#17)

--- plugins/util/filterAudioSections.js
+++ plugins/util/filterAudioSections.js
@@ -1,4 +1,4 @@
-const audioSectionNames = ['audioSection1', 'audioSection2']
+import { AUDIO_SECTION_NAMES } from '~/server/config'
 
-export default sections =>
-  sections.filter(section => audioSectionNames.includes(section.name))
+export default (sections = []) =>
+  sections.filter(section => AUDIO_SECTION_NAMES.includes(section.name))
--- server/api/index.js
+++ server/api/index.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
-// const _ = require('lodash')
 const logger = require('../logger')
 
 const { API_PROTOCOL, API_HOST, API_PORT, API_TIMEOUT } = require('../config')
 const apiURL = `${API_PROTOCOL}://${API_HOST}:${API_PORT}`
-const cache = require('./middle/redis')
+const { readMiddle, writeMiddle } = require('./middle/redis')
 
 // const debug = require('debug')('express:api/index')
 
@@ -15,24 +14,30 @@ const _axios = axios.create({
   timeout: API_TIMEOUT
 })
 
-router.get('*', cache.route(), (req, res, next) => {
-  _axios
-    .get(req.url)
-    .then(response => {
-      const { data, config } = response
+router.get(
+  '*',
+  readMiddle,
+  (req, res, next) => {
+    _axios
+      .get(req.url)
+      .then(response => {
+        const { data, config } = response
 
-      logger.info(`Fetch data from API url: ${config.url}`)
-      res.header('Cache-Control', 'public, max-age=300')
-      res.send(data)
-    })
-    .catch(error => {
-      const { config } = error
+        logger.info(`Fetch data from API url: ${config.url}`)
+        res.header('Cache-Control', 'public, max-age=300')
+        res.data = data
+        next()
+      })
+      .catch(error => {
+        const { config } = error
 
-      logger.error(
-        `Error occurred during fetching data from API url: ${config.url}`
-      )
-      next(new Error(error))
-    })
-})
+        logger.error(
+          `Error occurred during fetching data from API url: ${config.url}`
+        )
+        next(new Error(error))
+      })
+  },
+  writeMiddle
+)
 
 module.exports = router
