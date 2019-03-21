efd3d65a004fe93047d6f2618291e79e903ef95a
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-03-21 11:25:51 +0800

    feat: separate read/write from redis, get audio sections from config

--- server/api/middle/redis.js
+++ server/api/middle/redis.js
@@ -1,38 +1,98 @@
 /* eslint-disable import/order */
+const _ = require('lodash')
 const logger = require('../../logger')
 
 const {
   REDIS_READ_HOST,
   REDIS_READ_PORT,
-  // REDIS_WRITE_HOST,
-  // REDIS_WRITE_PORT,
+  REDIS_WRITE_HOST,
+  REDIS_WRITE_PORT,
   REDIS_AUTH,
-  REDIS_TIMEOUT
+  REDIS_TIMEOUT,
+  REDIS_KEY_PREFIX
   // REDIS_MAX_CLIENT,
   // REDIS_CONNECTION_TIMEOUT
 } = require('../../config')
 
-const cache = require('express-redis-cache')({
+const read = require('express-redis-cache')({
   host: REDIS_READ_HOST,
   port: REDIS_READ_PORT,
   auth_pass: REDIS_AUTH,
-  expire: REDIS_TIMEOUT
+  expire: REDIS_TIMEOUT,
+  prefix: REDIS_KEY_PREFIX
+})
+read.on('connected', () => {
+  logger.info('Redis Read Server connected')
+})
+read.on('disconnected', () => {
+  logger.info('Redis Read Server disconnected')
+})
+read.on('message', message => {
+  logger.info(`Redis Read Server message: ${message}`)
+})
+read.on('error', error => {
+  logger.error(`Redis Read Server error: ${error}`)
 })
 
-cache.on('connected', () => {
-  logger.info('Redis connected')
+const write = require('express-redis-cache')({
+  host: REDIS_WRITE_HOST,
+  port: REDIS_WRITE_PORT,
+  auth_pass: REDIS_AUTH,
+  expire: REDIS_TIMEOUT,
+  prefix: REDIS_KEY_PREFIX
+})
+write.on('connected', () => {
+  logger.info('Redis Write Server connected')
+})
+write.on('disconnected', () => {
+  logger.info('Redis Write Server disconnected')
+})
+write.on('message', message => {
+  logger.info(`Redis Write Server message: ${message}`)
+})
+write.on('error', error => {
+  logger.error(`Redis Write Server error: ${error}`)
 })
 
-cache.on('disconnected', () => {
-  logger.info('Redis disconnected')
-})
+const readMiddle = (req, res, next) => {
+  const entry = req.url
+  read.get(entry, (error, entries) => {
+    if (error) {
+      logger.error(
+        `Error occurred during fetching data from Redis Read Server, entry: ${entry}`
+      )
+      next(new Error(error))
+    }
 
-cache.on('message', message => {
-  logger.info(`Redis message: ${message}`)
-})
+    if (entries.length === 0 && !('body' in entries)) {
+      next()
+    } else {
+      const body = JSON.parse(_.get(entries, [0, 'body'], ''))
+      res.send(body)
+    }
+  })
+}
 
-cache.on('error', error => {
-  logger.error(`Redis error: ${error}`)
-})
+const writeMiddle = (req, res, next) => {
+  const entry = req.url
+  write.add(
+    entry,
+    JSON.stringify(res.data),
+    { expire: REDIS_TIMEOUT, type: 'json' },
+    error => {
+      if (error) {
+        logger.error(
+          `Error occurred during writing data to Redis Write Server, entry: ${entry}`
+        )
+        next(new Error(error))
+      }
 
-module.exports = cache
+      res.send(res.data)
+    }
+  )
+}
+
+module.exports = {
+  readMiddle,
+  writeMiddle
+}
