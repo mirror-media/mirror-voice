4bf9c580fb077533f639b86ca6e1f846be0b3f55
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-09-09 17:52:44 +0800

    feat: handle list and filter out audio-container

--- plugins/constants/sanitizeHtml.js
+++ plugins/constants/sanitizeHtml.js
@@ -1,3 +1,6 @@
 export default {
-  allowedTags: ['div', 'p', 'a']
+  allowedTags: ['div', 'p', 'a', 'ol', 'li'],
+  exclusiveFilter(frame) {
+    return frame.tag === 'div' && frame.attribs.class === 'audio-container '
+  }
 }
