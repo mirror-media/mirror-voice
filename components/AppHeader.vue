4ddb1f74b8118368ca29dc3729f268fd4d4e0b38
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-09-24 16:04:05 +0800

    feat(revamp): revamp header

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,24 +1,109 @@
 <template>
   <header class="header">
-    <nuxt-link to="/">
-      <img class="header__logo" src="~/assets/img/logo04.png" alt="">
-    </nuxt-link>
+    <nav class="header__main-navs main-navs">
+      <nuxt-link to="/">
+        <img
+          class="header__mv-logo"
+          src="~/assets/img/revamp/mirrorvoice_logo@2x.png"
+          alt=""
+        >
+      </nuxt-link>
+      <nav class="main-navs__section-navs section-navs">
+        <HeaderSectionNav
+          v-for="section in sections"
+          :key="section.id"
+          :section="section"
+          class="section-navs__nav"
+        />
+      </nav>
+    </nav>
+    <nav class="header__aside-navs aside-navs">
+      <a
+        href="https://www.mirrormedia.mg"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          class="header__mm-logo"
+          src="~/assets/img/revamp/mirrorvoice_media@2x.png"
+          alt=""
+        >
+      </a>
+      <a
+        href="https://www.mirrorfiction.com/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          class="header__mf-logo"
+          src="~/assets/img/revamp/mirrorfiction_logo@2x.png"
+          alt=""
+        >      
+      </a>
+    </nav>
   </header>
 </template>
 
+<script>
+import HeaderSectionNav from '~/components/Header/HeaderSectionNav.vue'
+
+import { mapState } from 'vuex'
+
+export default {
+  components: {
+    HeaderSectionNav
+  },
+  computed: {
+    ...mapState(['sections'])
+  }
+}
+</script>
+
 <style lang="stylus" scoped>
 .header
-  height 70px
-  background-color #d84939
-  padding 0 0 0 20px
+  z-index 1000
+  height 60px
+  background-color white
+  box-shadow 0px 3px 6px #00000029
+  padding 0 80px
+  display flex
+  justify-content space-between
+  align-items center
+  &__mv-logo
+    height 32px
+  &__mm-logo, &__mf-logo
+    height 20px
+
+.main-navs
   display flex
   align-items center
-  &__logo
-    // width 100px
-    height 35px
+  &__section-navs
+    margin 0 0 0 21px
+
+.section-navs
+  display flex
+  align-items center
+
+.aside-navs
+  a + a
+    margin 0 0 0 12px
 
 @media (max-width 768px)
   .header
-    height 72px
-    padding 0 0 0 14px
+    height 50px
+    padding 0 10px
+
+  .main-navs
+    width 100%
+    justify-content space-between
+    &__section-navs
+      margin 0 0 0 12px
+
+  .aside-navs
+    display none
+
+@media (max-width 320px)
+  .main-navs
+    &__section-navs
+      margin 0
 </style>
---
+++ store/index.js
@@ -0,0 +1,16 @@
+export const state = () => ({
+  sections: []
+})
+
+export const mutations = {
+  SET_SECTIONS(state, items) {
+    state.sections = items
+  }
+}
+
+export const actions = {
+  async nuxtServerInit({ commit }, { app }) {
+    const { items } = await app.$fetchSections({ max_results: 20 })
+    commit('SET_SECTIONS', items)
+  }
+}
