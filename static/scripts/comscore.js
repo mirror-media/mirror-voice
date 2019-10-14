77bd4d2435b16a3de91ea84c76a8580bec7b688d
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2019-10-14 12:08:05 +0800

    feat: add comscore script

---
+++ static/scripts/comscore.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+var _comscore = _comscore || []
+_comscore.push({ c1: '2', c2: '24318560' })
+;(function() {
+  const s = document.createElement('script')
+  const el = document.getElementsByTagName('script')[0]
+  s.async = true
+  s.src =
+    (document.location.protocol === 'https:' ? 'https://sb' : 'http://b') +
+    '.scorecardresearch.com/beacon.js'
+  el.parentNode.insertBefore(s, el)
+})()
