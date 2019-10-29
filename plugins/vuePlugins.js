5da9aa2010ca48edc5b0e662103883f4ef24ce91
Kai-Wen, Hsiung <lion15945@gmail.com>
2019-10-29 15:01:53 +0800

    feat: add abtest module and home showcase layout abtest (#49)
    * feat: add abtest module
    
    * feat: add home showcase layout abtest
    
    * fix: style in mobile
    
    * feat: enhance ab verification using query
    
    * chore: try to rebuild

--- plugins/vuePlugins.js
+++ plugins/vuePlugins.js
@@ -1,4 +1,13 @@
 import Vue from 'vue'
 import VueLazyload from 'vue-lazyload'
+import VueAB from './vue-a2b'
 
 Vue.use(VueLazyload)
+Vue.use(VueAB, {
+  storage: {
+    method: 'cookie',
+    isUniversal: false, // use cookie-universal for setting and getting cookie, avoid SSR issue
+    name: 'mirror-voice-abtest',
+    expiry: 7 * 2 // one week until the cookie expires
+  }
+})
