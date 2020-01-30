9e2da38ed2dac991126d3fa7d365cc71bb405f63
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-01-30 12:08:51 +0800

    feat(nuxt-plugin): refactor request plugin and add more request

--- plugins/requests/index.js
+++ plugins/requests/index.js
@@ -3,46 +3,53 @@ import Axios from './axios'
 
 export default ({ $axios }, inject) => {
   const _axios = new Axios($axios)
-
-  inject('fetchAudioPromotions', params => {
-    const query = buildQuery(params)
-    const url = `/api/audiopromotions?${query}`
-    return _axios
+  const get = url =>
+    _axios
       .get(url)
       .then(res => res)
       .catch(err => {
         throw err
       })
+
+  // Fetchs from api
+  inject('fetchAudioPromotions', params => {
+    const query = buildQuery(params)
+    const url = `/api/audiopromotions?${query}`
+    return get(url)
+  })
+  inject('fetchAudioChoices', params => {
+    const query = buildQuery(params)
+    const url = `/api/audiochoices?${query}`
+    return get(url)
   })
   inject('fetchSections', params => {
     const query = buildQuery(params)
     const url = `/api/sections?${query}`
-    return _axios
-      .get(url)
-      .then(res => res)
-      .catch(err => {
-        throw err
-      })
+    return get(url)
   })
   inject('fetchAlbums', params => {
     const query = buildQuery(params)
     const url = `/api/albums?${query}`
-    return _axios
-      .get(url)
-      .then(res => res)
-      .catch(err => {
-        throw err
-      })
+    return get(url)
   })
   inject('fetchSingle', params => {
     const query = buildQuery(params)
     const url = `/api/posts?${query}`
-    return _axios
-      .get(url)
-      .then(res => res)
-      .catch(err => {
-        throw err
-      })
+    return get(url)
+  })
+
+  // Fetchs from json on google cloud storage
+  inject('fetchPopularVoice', () => {
+    const url = '/api/json/popular_voice.json'
+    return get(url)
+  })
+  inject('fetchVoiceMasters', () => {
+    const url = '/api/rss/voice_masters.json'
+    return get(url)
+  })
+  inject('fetchCategoriesShowcase', () => {
+    const url = '/api/json/albums_posts_voice.json'
+    return get(url)
   })
 
   inject('postContact', params => {
