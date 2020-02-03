36ffd1349400410d981e68ae576d690e3244d698
Kai-Wen, Hsiung <kaiwenhsiung@gmail.com>
2020-02-03 11:50:37 +0800

    refactor(server): add prod, dev in-lined branching in redis

--- server/api/middle/redis.js
+++ server/api/middle/redis.js
@@ -14,45 +14,66 @@ const {
   // REDIS_CONNECTION_TIMEOUT
 } = require('../../config')
 
-const read = require('express-redis-cache')({
+const readRedis = require('express-redis-cache')({
   host: REDIS_READ_HOST,
   port: REDIS_READ_PORT,
   auth_pass: REDIS_AUTH,
   expire: REDIS_TIMEOUT,
   prefix: REDIS_KEY_PREFIX
 })
-read.on('connected', () => {
-  logger.info('[redis] Redis Read Server connected')
-})
-read.on('disconnected', () => {
-  logger.info('[redis] Redis Read Server disconnected')
-})
-read.on('message', message => {
-  logger.info(`[redis] Redis Read Server message: ${message}`)
-})
-read.on('error', error => {
-  logger.error(`[redis] Redis Read Server error: ${error}`)
-})
-
-const write = require('express-redis-cache')({
+const writeRedis = require('express-redis-cache')({
   host: REDIS_WRITE_HOST,
   port: REDIS_WRITE_PORT,
   auth_pass: REDIS_AUTH,
   expire: REDIS_TIMEOUT,
   prefix: REDIS_KEY_PREFIX
 })
-write.on('connected', () => {
-  logger.info('[redis] Redis Write Server connected')
-})
-write.on('disconnected', () => {
-  logger.info('[redis] Redis Write Server disconnected')
-})
-write.on('message', message => {
-  logger.info(`[redis] Redis Write Server message: ${message}`)
-})
-write.on('error', error => {
-  logger.error(`[redis] Redis Write Server error: ${error}`)
-})
+
+let read
+let write
+if (process.env.NODE_ENV === 'production') {
+  read = readRedis
+  read.on('connected', () => {
+    logger.info('[redis] Redis Read Server connected')
+  })
+  read.on('disconnected', () => {
+    logger.info('[redis] Redis Read Server disconnected')
+  })
+  read.on('message', message => {
+    logger.info(`[redis] Redis Read Server message: ${message}`)
+  })
+  read.on('error', error => {
+    logger.error(`[redis] Redis Read Server error: ${error}`)
+  })
+
+  write = writeRedis
+  write.on('connected', () => {
+    logger.info('[redis] Redis Write Server connected')
+  })
+  write.on('disconnected', () => {
+    logger.info('[redis] Redis Write Server disconnected')
+  })
+  write.on('message', message => {
+    logger.info(`[redis] Redis Write Server message: ${message}`)
+  })
+  write.on('error', error => {
+    logger.error(`[redis] Redis Write Server error: ${error}`)
+  })
+} else {
+  read = {
+    get(entry, callback) {
+      logger.info(`[redis] Skip the redis get in dev env of entry: ${entry}`)
+      callback(null, [])
+    }
+  }
+
+  write = {
+    add(entry, data, option, callback) {
+      logger.info(`[redis] Skip the redis write in dev env of entry: ${entry}`)
+      callback(null)
+    }
+  }
+}
 
 const readMiddle = (req, res, next) => {
   const entry = req.url
